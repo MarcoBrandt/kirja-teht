587c54e41ca1c27b4d77ede5bd14738aeac8af1c
MarcoBrandt <60316176+MarcoBrandt@users.noreply.github.com>
2021-04-28 21:39:36 +0300

    Node.js SQLite done

--- palvelin/kirjaDBcreate.js
+++ palvelin/kirjaDBcreate.js
@@ -1,12 +1,54 @@
 const sqlite3 = require('sqlite3').verbose();
 const db = new sqlite3.Database('kirjat.db');
 
-db.serialize ( () => {
-    let sql = 'CREATE TABLE Kirja (' + 
-    'id integer PRIMARY KEY NOT NULL, ' + 
-    'title text, ' +
-    'author text, ' +
-    'description text ) ';
+db.serialize( () => {
 
-    db.run(sql, (err) => { if (err) { return console.log(err.message) }})
-}) 
+	let sql = "CREATE TABLE kirja (" +
+			  "id integer PRIMARY KEY NOT NULL, " +
+			  "title text NOT NULL, " +
+			  "author text NOT NULL, " +
+			  "description text )";
+
+	db.run(sql, (err) => {
+		if (err) {
+		  return console.log(err.message);
+		}
+		console.log("Esimerkki taulu luotu");
+	});
+
+	sql = "INSERT INTO `kirja` (`id`, `title`, `author`, `description`) "+
+	" VALUES (1, 'The Lord of the Rings', 'J. R. R. Tolkien', 'Pls Return Ring')";
+	db.run(sql, (err) => {
+		if (err) {
+		  return console.log(err.message);
+		}
+		console.log("Rivi lisättiin");
+	});
+
+	sql = "INSERT INTO `kirja` (`id`, `title`, `author`, `description`) "+
+	" VALUES (2, 'Lord of the Flies', 'William Golding', 'Flies obey!')";
+	db.run(sql, (err) => {
+		if (err) {
+			return console.log(err.message);
+		}
+		console.log("Rivi lisättiin");
+	});
+
+	sql = "INSERT INTO `kirja` (`id`, `title`, `author`, `description`) "+
+	" VALUES (3, 'Lord of the Dance', 'Michael Flatley', 'Dance Dance Revolution')";
+	db.run(sql, (err) => {
+		if (err) {
+			return console.log(err.message);
+		}
+		console.log("Rivi lisättiin");
+	});
+
+	db.each("SELECT id, title FROM kirja", function(err, row) {
+		if (err) {
+		  return console.log(err.message);
+		}
+		console.log(row.id + ", " + row.title);
+	});
+
+	db.close();
+});
--- palvelin/kirjaServer.js
+++ palvelin/kirjaServer.js
@@ -1,16 +1,20 @@
 const express = require('express');
 const app = express();
+
+const bodyParser= require('body-parser')
+app.use(bodyParser.urlencoded({limit: '5mb', extended: true}))
+
+var helmet = require('helmet')
+app.use(helmet())
+
 app.use(express.json());
-express.urlencoded({limit: '5mb', extended: true});
+//express.urlencoded({limit: '5mb', extended: true});
 
 const cors = require('cors');
 app.use(cors());
 
-const helmet = require('helmet');
-app.use(helmet());
-
 const sqlite3 = require('sqlite3');
-const db = new sqlite3.Database('kirjat.db')
+const db = new sqlite3.Database('kirjat.db');
 
 app.listen(8080, () =>{
     console.log('Palvelin toimii osoitteessa localhost:8080');
@@ -22,7 +26,7 @@ app.get('/', (req, res, next) => {
 
 // Hae kaikki kirjat tietokannasta
 app.get('/kirja/all', (req, res, next) => {
-    db.all('SELECT * FROM Kirja', (error, results) => { // db.all eli haetaan kaikki
+    db.all('SELECT * FROM kirja', (error, results) => { // db.all eli haetaan kaikki
         if (error) throw error; // Virheen testausta
 
         /*if (typeof(result) == 'undefined') {
@@ -36,7 +40,7 @@ app.get('/kirja/all', (req, res, next) => {
 app.get('/kirja/unique/:id', (req, res, next) => { 
     let id = req.params.id; // Ottaa :id parametrin muuttujaksi
 
-    db.get('SELECT * FROM Kirja WHERE id=?', [id], (error, result) => { // db.get eli haetaa tietty tai tiettyjä
+    db.get('SELECT * FROM kirja WHERE id=?', [id], (error, result) => { // db.get eli haetaa tietty tai tiettyjä
         if (error) throw error; // Virheen testausta
 
         if (typeof(result) == 'undefined') {
@@ -50,7 +54,7 @@ app.get('/kirja/unique/:id', (req, res, next) => {
 app.get('/kirja/delete/:id', (req, res, next) => {
     let id = req.params.id; // Ottaa :id parametrin muuttujaksi
 
-    db.run('DELETE FROM Kirja WHERE id=?', [id], function (error, result) { //db.run eli ajetaan tietokantaan toiminto
+    db.run('DELETE FROM kirja WHERE id=?', [id], function (error, result) { //db.run eli ajetaan tietokantaan toiminto
         if (error) throw error; // Virheen testausta
 
         return res.status(200).json( {count: this.changes}); // Palautetaan muutuneiden rivien määrä
@@ -58,16 +62,19 @@ app.get('/kirja/delete/:id', (req, res, next) => {
 })
 
 // Tietokantaan kirjan lisäämminen
-app.post('/kirja/add', function (req, res) {
-    let tap = req.body;
-    //console.log(req);
-    console.log(tap);
-    
-    db.run('INSERT INTO Kirja (title, author, description) VALUES (?, ?, ?)', [tap.title, tap.author, tap.description], function (error, result) {
-        if (error) throw error;
 
-        return res.status(200).json({count: this.changes});
-    })
+var multer  = require('multer')
+var upload = multer()
+
+app.post('/kirja/add', upload.none() ,(req, res, next) => {
+	let kirja = req.body;
+
+    db.run('insert into kirja (title,author,description) values (?, ?, ?)',
+  		[kirja.title, kirja.author, kirja.description], (error, result) => {
+		if (error) throw error;
+
+		return res.status(200).json( {count: 1} );
+	});
 })
 
 app.get('*', (req, res, next) => {
